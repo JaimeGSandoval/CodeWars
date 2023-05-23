18c462ff6518e37f709dca669bc44297a9d95565
JaimeGSandoval <jaime.devs@gmail.com>
2023-05-23 13:34:58 -0700

    complete 3 array katas

---
+++ cascadingSibset.js
@@ -0,0 +1,25 @@
+// Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+
+// each_cons([1,2,3,4], 2)
+// #=> [[1,2], [2,3], [3,4]]
+
+// each_cons([1,2,3,4], 3)
+// #=> [[1,2,3],[2,3,4]]
+
+function eachCons(array, n) {
+  let cascadingSubset = [];
+  for (let i = 0; i <= array.length - n; i++) {
+    cascadingSubset.push(array.slice(i, i + n));
+  }
+  return cascadingSubset;
+}
+
+function eachCons(array, n) {
+  let res = [];
+
+  for (let i = 0; i < array.length; i++) {
+    res.push(array.slice(i, i + n));
+  }
+
+  return res.filter((e) => e.length === n);
+}
